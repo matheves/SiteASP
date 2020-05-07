1da107f493a846ba899e7f814578764ce533e615
matheves <michou38000@gmail.com>
2020-05-07 14:57:17 -0400

    Add files via upload

---
+++ WebApplication1/App_Code/UtilityClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.App_Code
+{
+    public class UtilityClass
+    {
+
+        public static string Hash_Me(string str_in)
+        {
+            string str_out = "";
+
+            for (int i = 0; i < str_in.Length; i++)
+            {
+                str_out += Convert.ToInt16(str_in[i]).ToString();
+            }
+
+            return str_out;
+        }
+    }
+}
